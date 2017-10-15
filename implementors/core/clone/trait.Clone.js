11b3ed3770f19bea2f26be80ad33f5476fc2ef30
Travis CI User <travis@example.org>
2017-10-15 23:52:05 +0000

    Documentation for japaric/criterion.rs@a31a0c4

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["criterion"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"criterion/struct.Bencher.html\" title=\"struct criterion::Bencher\">Bencher</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
